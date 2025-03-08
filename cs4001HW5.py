f4ab6af2738bdfafdf9ab6cad2d293c143add094
NolanRink <98193134+NolanRink@users.noreply.github.com>
2025-03-08 17:00:44 -0600

    changes

--- cs4001HW5.py
+++ cs4001HW5.py
@@ -1,6 +1,7 @@
 from mpi4py import MPI
 import numpy as np
 import time
+import sys
 import pandas as pd
 
 def matrix_multiply(A, B):
@@ -11,77 +12,76 @@ def matrix_multiply(A, B):
                 C[i][j] += A[i][k] * B[k][j]
     return C
 
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 parallel_options = [True, False]
-location = 'PC'
-sizes = [10, 100, 500, 1000]
+location = 'Cloudlab'
+sizes = [10, 100, 250, 500, 750]
 
-# Only master will collect results for output.
 times = []
 parallels = []
 locations = []
 mpi_sizes = []
 
 for parallel in parallel_options:
-for N in sizes:
-    start_time = time.time()
-    if parallel:
-        if rank == 0:
-            print(f"size: {size}")
-            # Master process: generate matrices and send parts to workers
+    for N in sizes:
+        start_time = time.time()
+        if parallel:
+            if rank == 0:
+                print(f"size: {size}")
+                # Master process: generate matrices and send parts to workers
+                A = np.random.rand(N, N)
+                B = np.random.rand(N, N)
+                chunk_size = A.shape[0] // size
+                A_chunks = [A[i:i+chunk_size] for i in range(0, A.shape[0], chunk_size)]
+                
+                # Send chunks to workers
+                for i in range(1, size):
+                    comm.send(A_chunks[i-1], dest=i, tag=1)
+                    comm.send(B, dest=i, tag=2)
+                
+                # Compute master's chunk
+                C_partial = matrix_multiply(A_chunks[0], B)
+                
+                # Gather results from workers
+                for i in range(1, size):
+                    C_partial += comm.recv(source=i, tag=3)
+            else:
+                # Worker processes: receive data, compute, and send result back
+                A_chunk = comm.recv(source=0, tag=1)
+                B = comm.recv(source=0, tag=2)
+                C_partial = matrix_multiply(A_chunk, B)
+                comm.send(C_partial, dest=0, tag=3)
+        else:
+            # Serial computation if parallel is False
+            print(f"size: 1")
             A = np.random.rand(N, N)
             B = np.random.rand(N, N)
-            chunk_size = A.shape[0] // size
-            A_chunks = [A[i:i+chunk_size] for i in range(0, A.shape[0], chunk_size)]
-            
-            # Send chunks to workers
-            for i in range(1, size):
-                comm.send(A_chunks[i-1], dest=i, tag=1)
-                comm.send(B, dest=i, tag=2)
-            
-            # Compute master's chunk
-            C_partial = matrix_multiply(A_chunks[0], B)
-            
-            # Gather results from workers
-            for i in range(1, size):
-                C_partial += comm.recv(source=i, tag=3)
-        else:
-            # Worker processes: receive data, compute, and send result back
-            A_chunk = comm.recv(source=0, tag=1)
-            B = comm.recv(source=0, tag=2)
-            C_partial = matrix_multiply(A_chunk, B)
-            comm.send(C_partial, dest=0, tag=3)
-    else:
-        # Serial computation if parallel is False
-        A = np.random.rand(N, N)
-        B = np.random.rand(N, N)
-        C = matrix_multiply(A, B)
-    end_time = time.time()
-    
-    # Only record times and print output on master
-    if rank == 0:
-        times.append(end_time - start_time)
-        parallels.append(parallel)
-        locations.append(location)
-        if parallel:
-            mpi_sizes.append(size)
-        else:
-            mpi_sizes.append(1)
-        print(f"Execution time: {end_time - start_time} seconds for N = {N}")
-        if not parallel:
-            print("Serial computation")
-        else:
-            print("Parallel computation")    
+            C = matrix_multiply(A, B)
+        end_time = time.time()
+        
+        # Only record times and print output on master
+        if rank == 0:
+            times.append(end_time - start_time)
+            parallels.append(parallel)
+            locations.append(location)
+            if parallel:
+                mpi_sizes.append(size)
+            else:
+                mpi_sizes.append(1)
+            print(f"Execution time: {end_time - start_time} seconds for N = {N} and cores = {size if parallel else 1}")
+            sys.stdout.flush()   
 # Only master writes the CSV file
 if rank == 0:
-df = pd.DataFrame({
-    'Size of N for NxN': sizes * len(parallel_options),
-    'Time': times,
-    'Parallelization': parallels,
-    'Location': locations,
-    'mpi_size': mpi_sizes
-})
-df.to_csv(f"Results_{location}.csv", index=False)
-print(f"Results written to Results_{location}.csv")
+    df = pd.DataFrame({
+        'Size of N for NxN': sizes * len(parallel_options),
+        'Time': times,
+        'Parallelization': parallels,
+        'Location': locations,
+        'mpi_size': mpi_sizes
+    })
+    df.to_csv(f"{location}Results.csv", index=False)
+    print(f"Results written to {location}Results.csv")
+ 
